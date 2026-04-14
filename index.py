74e4c7342f085c950a849a28664ea3dda931c92c
Kristian Øyen <kristianoyen1@gmail.com>
2026-04-14 15:32:39 +0200

    feat: added timestamp display and better session mapping

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
-
+from datetime import datetime
 
 def get_db():
     conn = sqlite3.connect("StudyTracker.db")
@@ -26,7 +26,8 @@ def init_db():
         CREATE TABLE IF NOT EXISTS sessions(
             id INTEGER PRIMARY KEY, 
             subject TEXT,
-            duration_minutes INTEGER
+            duration_minutes INTEGER,
+            created_at TEXT
         )
     """
     )
@@ -41,12 +42,14 @@ def startup():
 
 @app.get("/sessions")
 def get_sessions():
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM sessions")
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
+    with get_db() as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+    
+        query = "SELECT id, subject, duration_minutes, created_at FROM sessions"
+        cursor.execute(query)
+
+        return [dict(row) for row in cursor.fetchall()]
 
 
 @app.post("/sessions")
@@ -54,12 +57,13 @@ async def create_sessions(request: Request):
     data = await request.json()
     subject = data["subject"]
     duration_minutes = data["duration_minutes"]
+    created_at = datetime.now().strftime("%d.%m.%Y")
 
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO sessions (subject, duration_minutes) VALUES (?, ?)",
-        (subject, duration_minutes),
+        "INSERT INTO sessions (subject, duration_minutes, created_at) VALUES (?, ?, ?)",
+        (subject, duration_minutes, created_at),
     )
     conn.commit()
     conn.close()
